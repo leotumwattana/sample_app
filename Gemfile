36b451014c07d5737e5de46270a1db3783d4d59b
Leo Tumwattana <leo.tumwattana@gmail.com>
2013-10-13 00:48:39 +0800

    Finish static pages

--- Gemfile
+++ Gemfile
@@ -4,11 +4,17 @@ ruby '2.0.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.0'
+gem 'bundler'
 
 # Use sqlite3 as the database for Active Record
 group :development, :test do
+  gem 'rb-fsevent', :require => false if RUBY_PLATFORM =~ /darwin/i
   gem 'sqlite3', '1.3.8'
   gem 'rspec-rails', '2.13.1'
+  gem 'guard-rspec', '2.5.0'
+  gem 'spork-rails', '4.0.0'
+  gem 'guard-spork', '1.5.0'
+  gem 'childprocess', '0.3.6'
 end
 
 group :test do
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 SampleApp::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/help"
+  get "static_pages/about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
+  # You me in app/views/static_pages/help.html.erb can have the root of your site routed with "root"
   # root 'welcome#index'
 
   # Example of regular route:
